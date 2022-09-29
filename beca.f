df53ee8487a6a6e31469fe2559ca5da96a115e37
Angeldx356 <104656623+Angeldx356@users.noreply.github.com>
2022-09-28 20:57:58 -0500

    Update beca.f

--- beca.f
+++ beca.f
@@ -1,5 +1,4 @@
-        program beca
-        implicit none
+               implicit none
 
         integer edad;
         Real prom, bec;
@@ -11,29 +10,28 @@
         write(*,*) 'Promedio general actual'
         read(*,*)prom
 
-        if (edad > 18) then
+        if (edad >= 18) then
         if(prom >= 9) then
         bec = 2000
-        else if(prom>=7.5 & <=8.9) then
+        else if(prom >= 7.5 .AND. prom <= 8.9) then
         bec = 1000
-        else if(prom >= 6 & <=7.4) then
+        else if(prom >= 6 .AND. prom <= 7.4) then
         bec = 500
         else
-        write(*,*) 'Se te enviara una carta incitandote a mejorar y'
-        end if
-        
+        write(*,*) 'Se le invita a mejorar su calificacion'
+        endif
+
         else
         if(prom >= 9) then
         bec = 3000
-        else if(prom>=7.5 & <= 8.9) then
+        else if(prom >= 8.0 .AND. prom <= 8.9) then
         bec = 2000
-        else if(prom >= 6 & 7.4) then
+        else if(prom >= 6 .AND. prom <= 7.9) then
         bec = 1000
         else
         write(*,*) 'Se le invita a mejorar su calificacion'
-        end if
-        end if
-
+        endif
+        endif
         
         write(*,*) 'Tu beca es de $',bec,'.00'
         pause
